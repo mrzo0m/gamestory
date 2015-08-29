42d39eeb403bfb4bc36e006048a715cbb0f0d62b
mrzoom <mrzoom.214@gmail.com>
2015-08-29 21:26:22 +0300

    CQ init

---
+++ gamestory-services/src/main/java/com/epam/training/services/osgi/Activator.java
@@ -0,0 +1,19 @@
+package com.epam.training.services.osgi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squeakysand.osgi.framework.BasicBundleActivator;
+
+/**
+ * Bundle activator for com.epam.training - gamestory-services.
+ */
+public class Activator extends BasicBundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public Activator() {
+		super(LOG);
+	}
+
+}
---
+++ gamestory-view/src/main/content/jcr_root/apps/gamestory/components/examplecomponent/examplecomponent.jsp
@@ -0,0 +1,13 @@
+<%--
+
+    This is a simple non-page component using JSP that extends the foundation parbase component.
+
+--%>
+
+<%@ include file="/apps/gamestory/components/global.jspx" %>
+
+<%@ taglib prefix="eptr" uri="http://traning.epam.com/taglibs/gamestory-taglib" %>
+
+<p>This is an example non-page component (examplecomponent).</p>
+
+<h1><eptr:helloWorld name="CQ Blueprints User"/></h1>
